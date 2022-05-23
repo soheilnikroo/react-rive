55a413991723068e182978d23a73796023af194a
Soheil Nikroo <soheiln1234@gmail.com>
2022-05-23 20:59:00 +0430

    awseom animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import { useRive, useStateMachineInput } from 'rive-react';
+
+// @ts-ignore
+import SafeBoxIconAnimation from './assets/animation/safe-box-icon.riv';
 
 function App() {
+  const STATE_MACHINE_NAME = 'State Machine 1';
+  const INPUT_NAME = 'Pressed';
+
+  const { rive, RiveComponent } = useRive({
+    src: SafeBoxIconAnimation,
+    autoplay: true,
+    animations: 'Hover_in',
+    stateMachines: STATE_MACHINE_NAME,
+  });
+
+  const onClickInput = useStateMachineInput(
+    rive,
+    STATE_MACHINE_NAME,
+    INPUT_NAME
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#2f1e3e',
+      }}
+      onClick={() => onClickInput?.fire()}
+    >
+      <RiveComponent
+        height={'100vh'}
+        width={'100vw'}
+        style={{
+          width: '100vw',
+          height: '100vh',
+        }}
+      />
     </div>
   );
 }
